e8f2b9846e0d31750d690c78c2c1f05a0e7e1ed9
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2022-02-12 19:31:09 +0100

    Generate my-horizontal-elasticity-strategy-controller using `polaris-cli g elasticity-strategy-controller my-horizontal-elasticity-strategy-controller --eStratTypePkg=@my-org/my-strategies --eStratType=MyHorizontalElasticityStrategy`

---
+++ horizontal-elasticity-strat/apps/my-horizontal-elasticity-strategy-controller/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  displayName: 'my-horizontal-elasticity-strategy-controller',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory:
+    '../../coverage/apps/my-horizontal-elasticity-strategy-controller',
+};
---
+++ horizontal-elasticity-strat/apps/my-horizontal-elasticity-strategy-controller/src/app/elasticity/index.ts
@@ -0,0 +1,1 @@
+export * from './my-horizontal-elasticity-strategy.controller';

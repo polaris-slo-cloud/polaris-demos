b4c99d933297bcac0024d5e10903c032362441a3
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2022-02-12 18:39:49 +0100

    Generate MyHorizontalElasticityStrategy type using `polaris-cli g elasticity-strategy my-horizontal-elasticity-strategy --project=mystrategies --createLibProject=true --importPath=@my-org/my-strategies`

---
+++ horizontal-elasticity-strat/libs/mystrategies/src/lib/init-polaris-lib.ts
@@ -0,0 +1,12 @@
+import { PolarisRuntime } from '@polaris-sloc/core';
+import { MyHorizontalElasticityStrategy } from './elasticity/my-horizontal-elasticity-strategy.prm';
+
+/**
+ * Initializes this library and registers its types with the transformer in the `PolarisRuntime`.
+ */
+export function initPolarisLib(polarisRuntime: PolarisRuntime): void {
+  polarisRuntime.transformer.registerObjectKind(
+    new MyHorizontalElasticityStrategy().objectKind,
+    MyHorizontalElasticityStrategy
+  );
+}

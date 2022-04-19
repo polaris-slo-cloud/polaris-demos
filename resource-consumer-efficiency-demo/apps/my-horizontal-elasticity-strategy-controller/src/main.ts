debfadcd53fcfd998d8b517cb5e90dfdf0c232f7
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2022-04-19 13:25:37 +0200

    Generate my-horizontal-elasticity-strategy-controller

---
+++ resource-consumer-efficiency-demo/apps/my-horizontal-elasticity-strategy-controller/src/main.ts
@@ -0,0 +1,34 @@
+import { KubeConfig } from '@kubernetes/client-node';
+import {
+    MyHorizontalElasticityStrategyKind,
+    initPolarisLib as initMappingsLib,
+} from '@polaris-sloc/custom-elasticity-strategies';
+import { Logger } from '@polaris-sloc/core';
+import { initPolarisKubernetes } from '@polaris-sloc/kubernetes';
+import { MyHorizontalElasticityStrategyController } from './app/elasticity';
+
+// Load the KubeConfig and initialize the @polaris-sloc/kubernetes library.
+const k8sConfig = new KubeConfig();
+k8sConfig.loadFromDefault();
+const polarisRuntime = initPolarisKubernetes(k8sConfig);
+
+// Initialize the used Polaris mapping libraries
+initMappingsLib(polarisRuntime);
+
+// Create an ElasticityStrategyManager and watch the supported elasticity strategy kinds.
+const manager = polarisRuntime.createElasticityStrategyManager();
+manager
+    .startWatching({
+        kindsToWatch: [
+            {
+                kind: new MyHorizontalElasticityStrategyKind(),
+                controller: new MyHorizontalElasticityStrategyController(
+                    polarisRuntime
+                ),
+            },
+        ],
+    })
+    .catch((error) => {
+        Logger.error(error);
+        process.exit(1);
+    });

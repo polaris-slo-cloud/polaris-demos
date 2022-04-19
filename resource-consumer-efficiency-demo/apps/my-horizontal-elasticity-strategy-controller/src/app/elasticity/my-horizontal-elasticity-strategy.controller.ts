e05e96aa1c27c3eb9e852efe6cef82cfea2267a6
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2022-04-19 13:29:31 +0200

    Implement my-horizontal-elasticity-strategy-controller

--- resource-consumer-efficiency-demo/apps/my-horizontal-elasticity-strategy-controller/src/app/elasticity/my-horizontal-elasticity-strategy.controller.ts
+++ resource-consumer-efficiency-demo/apps/my-horizontal-elasticity-strategy-controller/src/app/elasticity/my-horizontal-elasticity-strategy.controller.ts
@@ -1,68 +1,30 @@
 import {
-    DefaultStabilizationWindowTracker,
-    OrchestratorClient,
-    PolarisRuntime,
-    SloComplianceElasticityStrategyControllerBase,
+    ElasticityStrategy,
+    HorizontalElasticityStrategyControllerBase,
+    Scale,
+    SloCompliance,
     SloTarget,
-    StabilizationWindowTracker,
 } from '@polaris-sloc/core';
 import {
     MyHorizontalElasticityStrategyConfig,
-    MyHorizontalElasticityStrategy,
 } from '@polaris-sloc/custom-elasticity-strategies';
-
-/** Tracked executions eviction interval of 20 minutes. */
-const EVICTION_INTERVAL_MSEC = 20 * 60 * 1000;
 
 /**
  * Controller for the MyHorizontalElasticityStrategy.
- *
- * ToDo:
- *  1. If you want to restrict the type of workloads that this elasticity strategy can be applied to,
- *     change the first generic parameter from `SloTarget` to the appropriate type.
- *  2. If your elasticity strategy input is not of type `SloCompliance`, change the definition of the controller class
- *     to extend `ElasticityStrategyController` instead of `SloComplianceElasticityStrategyControllerBase`.
- *  3. Implement the `execute()` method.
- *  4. Adapt `manifests/1-rbac.yaml` to include get and update permissions on all resources that you update in the orchestrator during `execute()`.
  */
-export class MyHorizontalElasticityStrategyController extends SloComplianceElasticityStrategyControllerBase<
+export class MyHorizontalElasticityStrategyController extends HorizontalElasticityStrategyControllerBase<
     SloTarget,
     MyHorizontalElasticityStrategyConfig
 > {
-    /** The client for accessing orchestrator resources. */
-    private orchClient: OrchestratorClient;
-
-    /** Tracks the stabilization windows of the ElasticityStrategy instances. */
-    private stabilizationWindowTracker: StabilizationWindowTracker<MyHorizontalElasticityStrategy> =
-        new DefaultStabilizationWindowTracker();
-
-    private evictionInterval: NodeJS.Timeout;
-
-    constructor(polarisRuntime: PolarisRuntime) {
-        super();
-        this.orchClient = polarisRuntime.createOrchestratorClient();
-
-        this.evictionInterval = setInterval(
-            () => this.stabilizationWindowTracker.evictExpiredExecutions(),
-            EVICTION_INTERVAL_MSEC
-        );
-    }
-
-    async execute(
-        elasticityStrategy: MyHorizontalElasticityStrategy
-    ): Promise<void> {
-        // ToDo: Implement this method
-    }
-
-    onDestroy(): void {
-        clearInterval(this.evictionInterval);
-    }
-
-    onElasticityStrategyDeleted(
-        elasticityStrategy: MyHorizontalElasticityStrategy
-    ): void {
-        this.stabilizationWindowTracker.removeElasticityStrategy(
-            elasticityStrategy
-        );
+    protected computeScale(
+        elasticityStrategy: ElasticityStrategy<SloCompliance, SloTarget, MyHorizontalElasticityStrategyConfig>,
+        currScale: Scale,
+    ): Promise<Scale> {
+        const newScale = new Scale(currScale);
+        const compliance = elasticityStrategy.spec.sloOutputParams.currSloCompliancePercentage;
+        const multiplier = compliance / 100;
+        newScale.spec.replicas = Math.ceil(currScale.spec.replicas * multiplier);
+        console.log(`Old replicas: ${currScale.spec.replicas}, new replicas: ${currScale.spec.replicas}, compliance: ${compliance}, multiplier: ${multiplier}`)
+        return Promise.resolve(newScale);
     }
 }

dbfea93baebd80db514a18af750350e3e0b49c3b
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2022-04-19 13:21:40 +0200

    Fill out MyHorizontalElasticityStrategyConfig

--- resource-consumer-efficiency-demo/libs/custom-elasticity-strategies/src/lib/elasticity/my-horizontal-elasticity-strategy.prm.ts
+++ resource-consumer-efficiency-demo/libs/custom-elasticity-strategies/src/lib/elasticity/my-horizontal-elasticity-strategy.prm.ts
@@ -6,20 +6,22 @@ import {
     initSelf,
 } from '@polaris-sloc/core';
 
-// ToDo after code generation:
-// - Add configuration parameters to the MyHorizontalElasticityStrategyConfig interface.
-// - If the elasticity strategy does not take SloCompliance objects as input,
-//   adapt the first generic parameter of MyHorizontalElasticityStrategyKind and MyHorizontalElasticityStrategy accordingly.
-// - If the elasticity strategy should operate on a subtype of SloTarget,
-//   adapt the second generic parameter of MyHorizontalElasticityStrategyKind and MyHorizontalElasticityStrategy accordingly.
-// - (optional) Replace the ObjectKind.group in the constructor of MyHorizontalElasticityStrategy with your own.
-//   If you change the group name, ensure that you also accordingly adapt the `1-rbac.yaml` files of all
-//   the elasticity strategy controller that needs to read and SLO controllers that need to write this ElasticityStrategy CRD.
-
 /**
  * Configuration options for MyHorizontalElasticityStrategy.
  */
-export interface MyHorizontalElasticityStrategyConfig {}
+export interface MyHorizontalElasticityStrategyConfig {
+
+    /**
+     * The minimum number of replicas that the target workload must have.
+     */
+    minReplicas?: number;
+
+    /**
+     * The maximum number of replicas that the target workload must have.
+     */
+    maxReplicas?: number;
+
+}
 
 /**
  * Denotes the elasticity strategy kind for the MyHorizontalElasticityStrategy.
